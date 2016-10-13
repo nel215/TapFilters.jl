daf86c4c36228cd710276cf7a63a074a7a61be01
nel215 <otomo.yuhei@gmail.com>
2016-10-13 23:06:04 +0900

    Add 5-tap filter

---
+++ test/runtests.jl
@@ -0,0 +1,11 @@
+module NTapFiltersTests
+
+using NTapFilters, Base.Test
+
+@test size(dx) == (5,5)
+@test size(dy) == (5,5)
+@test size(dxx) == (5,5)
+@test size(dyy) == (5,5)
+@test size(dxy) == (5,5)
+
+end
